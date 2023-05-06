dbfac781daf94aeff09c9450ab1e485bfe0843a5
sergiorgomar <gomarsergio34@gmail.com>
2023-05-05 19:54:03 -0600

    Update: creating the first window of the game

--- index.py
+++ index.py
@@ -1,4 +1,13 @@
-import pygame
+import pygame, sys
+
 pygame.init()
 
+size = (500,500)
+screen = pygame.display.set_mode(size)
 
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
